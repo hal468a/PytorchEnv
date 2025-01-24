06efbda80dd16274cd3284c7cc0028fda4f40073
hal468a <asdfg880226@gmail.com>
2025-01-24 23:30:41 +0800

    update

--- gpuTest.sh
+++ gpuTest.sh
@@ -1,1 +1,6 @@
+echo -e "------ cuda container test ---------"
 docker run --gpus all --rm nvidia/cuda:12.4.0-base-ubuntu22.04 nvidia-smi
+echo -e "\n"
+
+echo -e "------ pytorch container test ------"
+docker compose run --rm pytorch
